bcb238b88852244e2e04829ffef5358b826d044c
georginaw <watson.georgina@gmail.com>
2019-09-23 15:14:46 +0100

    added query to pull the film titles and attributes from the database

--- index.php
+++ index.php
@@ -4,3 +4,5 @@ $db = new PDO('mysql:host=db;dbname=collection', 'root', 'password');
 
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
+$db->query('SELECT `title`, `release_year`, `my_review`, `bechdel_status` FROM `collection`');
+
