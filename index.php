19c1f71094980ffc6baa39b3ad493763842dbd7b
georginaw <watson.georgina@gmail.com>
2019-09-23 15:44:42 +0100

    edited line 9 to select from table 'films' rather than the whole database 'collection'

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $db = new PDO('mysql:host=db;dbname=collection', 'root', 'password');
 
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$query = $db->query('SELECT `title`, `release_year`, `my_review`, `bechdel_status` FROM `collection`');
+$query = $db->query('SELECT `title`, `release_year`, `my_review`, `bechdel_status` FROM `films`');
 
 $films = $query->fetchAll();
 
