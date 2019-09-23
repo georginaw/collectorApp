621d97219c982babcb57e2e00d286cd63e81e337
georginaw <watson.georgina@gmail.com>
2019-09-23 15:27:44 +0100

    fetched data from query and created var_dump

--- index.php
+++ index.php
@@ -4,5 +4,9 @@ $db = new PDO('mysql:host=db;dbname=collection', 'root', 'password');
 
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$db->query('SELECT `title`, `release_year`, `my_review`, `bechdel_status` FROM `collection`');
+$query = $db->query('SELECT `title`, `release_year`, `my_review`, `bechdel_status` FROM `collection`');
+
+$films = $query->fetchAll();
+
+var_dump($films);
 
