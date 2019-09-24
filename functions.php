70e13e3027e8d1030d04f66202d0125117fc3f7f
georginaw <watson.georgina@gmail.com>
2019-09-24 14:03:22 +0100

    took out inline styles, created css stylesheet, linked it into index.php by creating boilerplate html inside the file. Added a normalize stylesheet. Put styles back onto p and h3 tags which were inline before

--- functions.php
+++ functions.php
@@ -13,9 +13,9 @@ function displayFilms(array $array) : string {
     $string = '';
     foreach ($array as $item) {
         $string .=
-            '<h3 style="margin-bottom: 5px">' . $item['title'] . '</h3>' .
-            '<p style="margin:0">' . 'Year released: ' . $item['release_year'] . '</p>' .
-            '<p style="margin:0">' . 'Star review: ' . $item['my_review'] . '</p>' .
-            '<p style="margin:0">' . 'Bechdel test: ' . $item['bechdel_status'] . '</p>';
+            '<h3>' . $item['title'] . '</h3>' .
+            '<p>' . 'Year released: ' . $item['release_year'] . '</p>' .
+            '<p>' . 'Star review: ' . $item['my_review'] . '</p>' .
+            '<p>' . 'Bechdel test: ' . $item['bechdel_status'] . '</p>';
     } return $string;
 }
--- index.php
+++ index.php
@@ -10,7 +10,27 @@ $query = $db->query('SELECT `title`, `release_year`, `my_review`, `bechdel_statu
 
 $films = $query->fetchAll();
 
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+    <link href="normalize.css" rel="stylesheet" type="text/css">
+    <link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<?php
+
 echo displayFilms($films);
+
+?>
+</body>
+</html>
+
+
 
 
 
