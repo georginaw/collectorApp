b819c05ab9040761fc12463940d53c565d62406a
georginaw <watson.georgina@gmail.com>
2019-09-24 11:38:38 +0100

    updated the foreach loop by putting it into a function which takes an array as it's parameter

--- functions.php
+++ functions.php
@@ -2,9 +2,13 @@
 
 require_once ('index.php');
 
-foreach ($films as $listing) {
-echo '<h3 style="margin-bottom: 5px">' . $listing['title'] . '</h3>';
-echo '<p style="margin:0">' . 'Year released: ' . $listing['release_year'] . '</p>';
-echo '<p style="margin:0">' . 'Star review: ' . $listing['my_review'] . '</p>';
-echo '<p style="margin:0">' . 'Bechdel test: ' . $listing['bechdel_status'] . '</p>';
+function displayFilms($array) {
+    foreach ($array as $item) {
+        echo '<h3 style="margin-bottom: 5px">' . $item['title'] . '</h3>';
+        echo '<p style="margin:0">' . 'Year released: ' . $item['release_year'] . '</p>';
+        echo '<p style="margin:0">' . 'Star review: ' . $item['my_review'] . '</p>';
+        echo '<p style="margin:0">' . 'Bechdel test: ' . $item['bechdel_status'] . '</p>';
+    }
+
+
 }
