9b62d64cd71b4c0f9649161e453f52011ab91e9c
georginaw <watson.georgina@gmail.com>
2019-09-24 11:39:20 +0100

    type-hinted the array for the 'displayData' function

--- functions.php
+++ functions.php
@@ -2,13 +2,11 @@
 
 require_once ('index.php');
 
-function displayFilms($array) {
+function displayFilms(array $array) {
     foreach ($array as $item) {
         echo '<h3 style="margin-bottom: 5px">' . $item['title'] . '</h3>';
         echo '<p style="margin:0">' . 'Year released: ' . $item['release_year'] . '</p>';
         echo '<p style="margin:0">' . 'Star review: ' . $item['my_review'] . '</p>';
         echo '<p style="margin:0">' . 'Bechdel test: ' . $item['bechdel_status'] . '</p>';
     }
-
-
 }
