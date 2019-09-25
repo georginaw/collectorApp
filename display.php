7493a8d0d09e7a95141c8b8c9bcadf0198429ab0
georginaw <watson.georgina@gmail.com>
2019-09-25 13:01:59 +0100

    called the function in the display page which retrieves data from the db

--- display.php
+++ display.php
@@ -2,13 +2,7 @@
 
 require_once('functions.php');
 
-$db = new PDO('mysql:host=db;dbname=collection', 'root', 'password');
-
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-$query = $db->query('SELECT `title`, `release_year`, `my_review`, `bechdel_status` FROM `films`');
-
-$films = $query->fetchAll();
+$films = fetchFromDB();
 
 ?>
 
