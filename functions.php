83a6a949400ec55a3d663e7312c2c6a9be3e53aa
georginaw <watson.georgina@gmail.com>
2019-09-25 10:15:34 +0100

    updated the unit test function (updated to indexed multidimensional array with assoc array inside), updated the functions string so that the unit test works...

--- functions.php
+++ functions.php
@@ -13,9 +13,6 @@ function displayFilms(array $films) : string {
     $string = '';
     foreach ($films as $film) {
         $string .=
-            '<h3>' . $film['title'] . '</h3>
-            <p>Year released: ' . $film['release_year'] . '</p>
-            <p>Star review: ' . $film['my_review'] . '</p>
-            <p>Bechdel test: ' . $film['bechdel_status'] . '</p>';
+            '<h3>' . $film['title'] . '</h3><p>Year released: ' . $film['release_year'] . '</p><p>Star review: ' . $film['my_review'] . '</p><p>Bechdel test: ' . $film['bechdel_status'] . '</p>';
     } return $string;
 }
