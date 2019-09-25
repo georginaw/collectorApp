7375b3960873c8edbdb90fb66707063952eae2da
georginaw <watson.georgina@gmail.com>
2019-09-25 11:22:44 +0100

    editing form a bit, updated link to bechdel search engine

--- form.php
+++ form.php
@@ -15,14 +15,15 @@
         <label>Film Title:</label>
         <input type="text" name="title">
         <label>Year released:</label>
-        <input type="number" maxlength="4" minlength="4" name="release_year">
+        <input type="number" max="2020" min="1900" name="release_year">
         <label>Review /5:</label>
         <input type="number" maxlength="1" minlength="1" max="5" min="0" name="my_review">
-        <a href="https://en.wikipedia.org/wiki/Bechdel_test"><label>Bechdel Test:</label></a>
+        <a href="http://bechdeltest.com/search/"><label>Bechdel Test:</label></a>
         <select name="bechdel_status" value="Select:">
               <option value="PASS">Pass</option>
               <option value="FAIL">Fail</option>
-          </select>
+        </select>
+        <input type="submit" value="Add film to collection" name="submit-to-db">
     </form>
 
 <?php
