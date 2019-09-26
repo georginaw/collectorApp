24797472e5a7de62334dc4451677a1c9408a3ac8
georginaw <watson.georgina@gmail.com>
2019-09-26 18:59:22 +0100

    updated the test function to include new div and form info, unit tested it and it works

--- test/functions.php
+++ test/functions.php
@@ -7,7 +7,7 @@ require_once ('../functions.php');
 class FunctionsTest extends TestCase {
     public function testDisplayFilms() {
         $arrayData = [['id' => 1, 'title' => 'A Few Good Men', 'release_year' => 1992, 'my_review' => 4, 'bechdel_status' => 'FAIL'], ['id' => 2, 'title' => 'Stand By Me', 'release_year' => 1986, 'my_review' => 5, 'bechdel_status' => 'FAIL']];
-        $expectedResult = '<div class="film-card id-1"><h3>A Few Good Men</h3><p>Year released: 1992</p><p>Star review: 4</p><p>Bechdel test: FAIL</p></div><div class="film-card id-2"><h3>Stand By Me</h3><p>Year released: 1986</p><p>Star review: 5</p><p>Bechdel test: FAIL</p></div>';
+        $expectedResult = '<div class="film-card"><div class="film-detail-box"><h3>A Few Good Men</h3><p>Year released: 1992</p><p>Star review: 4</p><p>Bechdel test: FAIL</p></div><form method="post" action="delete-from-database.php"><input type="submit" value="Delete" name="1"></form></div><div class="film-card"><div class="film-detail-box"><h3>Stand By Me</h3><p>Year released: 1986</p><p>Star review: 5</p><p>Bechdel test: FAIL</p></div><form method="post" action="delete-from-database.php"><input type="submit" value="Delete" name="2"></form></div>';
         $result = displayFilms($arrayData);
         $this->assertEquals($expectedResult,$result);
     }
